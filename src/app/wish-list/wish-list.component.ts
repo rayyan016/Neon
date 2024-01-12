0f9f719e1baab07a3bcd936bfd06e816ed1d2e97
Rayyan <rayyanaslam16@gmail.com>
2024-01-13 03:00:56 +0530

    Refactor wish-list component to use wish-list-item component

--- src/app/wish-list/wish-list.component.ts
+++ src/app/wish-list/wish-list.component.ts
@@ -2,18 +2,15 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WishItem } from '../../shared/models/wishItems';
+import { WishListItemComponent } from '../wish-list-item/wish-list-item.component';
 
 @Component({
   selector: 'wish-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, WishListItemComponent],
   templateUrl: './wish-list.component.html',
   styleUrl: './wish-list.component.css',
 })
 export class WishListComponent {
   @Input() wishes: WishItem[] = [];
-
-  toggleItem(item: WishItem) {
-    item.isComplete = !item.isComplete;
-  }
 }
