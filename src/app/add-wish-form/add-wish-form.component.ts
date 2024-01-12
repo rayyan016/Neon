4deeb8b7631c5bd2abcd7d2e13a8c41621cf24ca
Rayyan <rayyanaslam16@gmail.com>
2024-01-12 17:10:29 +0530

    Add AddWishFormComponent to the project

---
+++ src/app/add-wish-form/add-wish-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { WishItem } from '../../shared/models/wishItems';
+
+@Component({
+  selector: 'add-wish-form',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './add-wish-form.component.html',
+  styleUrl: './add-wish-form.component.css',
+})
+export class AddWishFormComponent {
+  @Output() addWish = new EventEmitter<WishItem>();
+
+  newWishText = '';
+
+  addNewWish() {
+    this.addWish.emit(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
+}
