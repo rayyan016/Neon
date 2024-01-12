09cefc427766a04d860fa7ace204d03fec924c45
Rayyan <rayyanaslam16@gmail.com>
2024-01-12 16:52:48 +0530

    Add wish-list component with template and logic

---
+++ src/app/wish-list/wish-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WishItem } from '../../shared/models/wishItems';
+
+@Component({
+  selector: 'wish-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './wish-list.component.html',
+  styleUrl: './wish-list.component.css',
+})
+export class WishListComponent {
+  @Input() wishes: WishItem[] = [];
+
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
+}
