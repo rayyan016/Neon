b8c256725f745b1986537630e5651043da7db131
Rayyan <rayyanaslam16@gmail.com>
2024-01-13 23:17:34 +0530

    Add CommonModule import for ngClass

--- src/app/wish-list-item/wish-list-item.component.ts
+++ src/app/wish-list-item/wish-list-item.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'wish-list-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './wish-list-item.component.html',
   styleUrl: './wish-list-item.component.css',
 })
