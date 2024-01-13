0ebe5d3fa7025e7665f764dfc962c49c7c9a6b44
Rayyan <rayyanaslam16@gmail.com>
2024-01-14 00:30:51 +0530

    Add event listener for 'removeWish' event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { WishListComponent } from './wish-list/wish-list.component';
 import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
 
+import events from './../shared/services/EventService';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -28,5 +30,12 @@ export class AppComponent {
     new WishItem('Learn React', true),
     new WishItem('Find a senzu bean'),
   ];
+
+  constructor() {
+    events.listen('removeWish', (wish: any) => {
+      console.log(wish);
+    });
+  }
+
   filter: any;
 }
