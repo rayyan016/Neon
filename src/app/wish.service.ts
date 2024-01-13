072dcbfd8702e5fb83d8fe53db3173adee4346e5
Rayyan <rayyanaslam16@gmail.com>
2024-01-14 02:00:45 +0530

    Update WishService to include HttpParams

--- src/app/wish.service.ts
+++ src/app/wish.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { WishItem } from '../shared/models/wishItems';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WishService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  private getStandarOptions(): any {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+  }
 
   getWishes() {
-    return this.http.get('assets/wishes.json');
+    let options = this.getStandarOptions();
+    options.params = new HttpParams({
+      fromObject: {
+        format: 'json',
+      },
+    });
+
+    return this.http.get('assets/wishes.json', options);
   }
+
 }
