854191c74f8dec1f4c493a8bea6343185228d834
Rayyan <rayyanaslam16@gmail.com>
2024-01-14 19:41:52 +0530

    Add form controls and submitForm method to ContactComponent

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
+  senderNameControl = new FormControl();
+  senderEmailControl = new FormControl();
+  senderMessageControl = new FormControl();
 
+  submitForm() {
+    if (this.senderNameControl.dirty) {
+      console.log('Change');
+    }
+  }
 }
