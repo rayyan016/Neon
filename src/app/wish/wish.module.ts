a7fe52c700680cb71459448e5c4c072d37583e6d
Rayyan <rayyanaslam16@gmail.com>
2024-01-14 15:14:40 +0530

    Refactor module

--- src/app/wish/wish.module.ts
+++ src/app/wish/wish.module.ts
@@ -1,23 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { WishListComponent } from './wish-list/wish-list.component';
-import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
-import { WishFilterComponent } from './wish-filter/wish-filter.component';
-
+import { WishComponent } from './wish.component';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    WishListComponent,
-    AddWishFormComponent,
-    WishFilterComponent,
-  ],
-  exports: [
-    WishListComponent,
-    AddWishFormComponent,
-    WishFilterComponent,
-  ],
+  imports: [WishComponent],
+  exports: [WishComponent],
 })
-export class WishModule { }
+export class WishModule {}
