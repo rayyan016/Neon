c71131f7e7877636eac56e288297b563626fe215
Rayyan <rayyanaslam16@gmail.com>
2024-01-15 00:08:35 +0530

    Update contact form validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,22 +1,26 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
 export class ContactComponent {
   contactForm = new FormGroup({
-    senderName: new FormControl(''),
-    senderEmail: new FormControl(''),
-    senderMessage: new FormControl(''),
+    senderName: new FormControl('', Validators.required),
+    senderEmail: new FormControl('', [Validators.required, Validators.email]),
+    senderMessage: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
   });
 
   submitForm() {
-    console.log(this.contactForm.value);
+    console.log(this.contactForm.valid);
   }
 }
