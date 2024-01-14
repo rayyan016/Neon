9b56aaafe38f4b88c10f4e67cef6eb735d252795
Rayyan <rayyanaslam16@gmail.com>
2024-01-14 15:14:56 +0530

    Refactor app component and remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { WishModule } from './wish/wish.module';
-import { WishItem } from '../shared/models/wishItems';
-
-import { EventService } from './../shared/services/EventService';
-import { WishService } from './wish/wish.service';
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,26 +8,4 @@ import { WishService } from './wish/wish.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-  items: WishItem[] = [];
-
-  constructor(events: EventService, private wishService: WishService) {
-    events.listen('removeWish', (wish: any) => {
-      const index = this.items.indexOf(wish);
-      this.items.splice(index, 1);
-    });
-  }
-
-  ngOnInit(): void {
-    this.wishService.getWishes().subscribe(
-      (data: any) => {
-        this.items = data;
-      },
-      (error: any) => {
-        alert(error.message);
-      }
-    );
-  }
-
-  filter: any;
-}
+export class AppComponent {}
