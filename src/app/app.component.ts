b37b8bb9e218360b46aaca7ef53dc3a9566c3bb7
Rayyan <rayyanaslam16@gmail.com>
2024-01-14 19:42:12 +0530

    Add ContactModule to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 import { WishModule } from './wish/wish.module';
+import { ContactModule } from './contact/contact.module';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [WishModule],
+  imports: [WishModule, ContactModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
